178d72770f6c57ccd41b1e57a80e388b2aabbd25
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-07-04 13:40:44 +0100

    Added code to add Analysis to Submission Envelope.

--- core/analysis_file_reference.py
+++ core/analysis_file_reference.py
@@ -12,7 +12,24 @@ class AnalysisFileReferenceTest(TestCase):
 
     def test_no_duplication(self):
         access_token = self._sign_on()
-        processes_links = self._prepare_submission(access_token)
+        processes_link = self._prepare_submission(access_token)
+
+        analysis_request = {
+            'process_core': {
+                'process_id': 'test_process_1'
+            },
+            'schema_type': 'process',
+            'describedBy': 'https://schema.humancellatlas.org/type/process/1.0.0/process'
+        }
+        response = requests.post(processes_link, json=analysis_request)
+        links = response.json().get('_links')
+        add_file_reference_link = links.get('add-file-reference')
+        self.assertTrue(add_file_reference_link, 'Link to adding File reference expected.')
+        derived_files_link = links.get('derivedFiles')
+        self.assertTrue(derived_files_link, 'Link to get derived Files expected.')
+        derived_files_link = derived_files_link.get('href')
+
+        self._assert_resource_count(derived_files_link, 0)
 
     def _sign_on(self):
         request_body = {
@@ -32,8 +49,13 @@ class AnalysisFileReferenceTest(TestCase):
     def _prepare_submission(self, access_token):
         headers = {'Authorization': f'Bearer {access_token}'}
         response = requests.post(f'{BASE_API_URL}/submissionEnvelopes', headers=headers, json={})
-        links = response.json()['_links']
+        links = response.json().get('_links')
         self.assertTrue(links, 'Links expected from API response.')
-        processes_links = links['processes']
-        self.assertTrue(processes_links, 'Link to Process resource expected from API response.')
-        return processes_links
+        processes_link = links.get('processes')
+        self.assertTrue(processes_link, 'Link to Process resource expected from API response.')
+        return processes_link.get('href')
+
+    def _assert_resource_count(self, derived_files_link, total_count):
+        response = requests.get(derived_files_link)
+        page_details = response.json().get('page')
+        self.assertEqual(total_count, page_details.get('totalElements'))
