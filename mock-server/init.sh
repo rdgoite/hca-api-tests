6a6af49f431612c5b72704c9ecf43dcd62e1a2f7
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-10 14:06:11 +0100

    Modified mock server to use envs to setup response header and body.

--- mock-server/init.sh
+++ mock-server/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 while true; do
-    (cat server/response_header.txt; echo -e "\n$RESPONSE_BODY" ) | nc -q 0 -lp $1
+    (echo -e $RESPONSE_HEADER; echo -e "\n$RESPONSE_BODY" ) | nc -q 0 -lp $1
 done
