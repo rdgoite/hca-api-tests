6d7bf6afc3f42e7f17074cf155a72b10c4271a09
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-13 14:17:45 +0100

    Created mock upload service project.

---
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/MockUploadServiceApplication.java
@@ -0,0 +1,12 @@
+package ingest.humancellatlas.org.mockuploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockUploadServiceApplication.class, args);
+	}
+}
