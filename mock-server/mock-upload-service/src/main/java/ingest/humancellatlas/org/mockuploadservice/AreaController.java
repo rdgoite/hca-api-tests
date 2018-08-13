c727ee5b598b52c88c7cf03c3e1444bb80865eae
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-13 14:40:45 +0100

    Defined controller method for handling request to validate file.

--- mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,16 @@ public class AreaController {
         ObjectNode response = objectMapper.createObjectNode();
         String uploadAreaUuid = UUID.randomUUID().toString();
         response.put("uri", format("s3://org-humancellatlas-upload-dev/%s/", uploadAreaUuid));
+        //TODO set to return 201 created response
         return response;
+    }
+
+    @PutMapping("/{uploadAreaUuid}/{fileName}/validate")
+    public ResponseEntity validateFile(String uploadAreaUuid, String fileName) {
+        String validationId = UUID.randomUUID().toString();
+        ObjectNode response = objectMapper.createObjectNode();
+        response.put("validation_id", validationId);
+        return ResponseEntity.ok().body(response);
     }
 
 }
