ebd95ca67c1f0907ab371edd2c3b27451b443210
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-13 10:23:29 +0100

    Created Spring code base for AMQP mock service.

---
+++ mock-server/mock-amqp-service/src/main/java/ingest/humancellatlas/org/mockamqpservice/MockAmqpServiceApplication.java
@@ -0,0 +1,12 @@
+package ingest.humancellatlas.org.mockamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockAmqpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockAmqpServiceApplication.class, args);
+	}
+}
