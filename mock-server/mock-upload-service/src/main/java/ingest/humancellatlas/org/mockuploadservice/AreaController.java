578b9e5bfcbed096258e99940416ec758b049062
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-14 14:40:09 +0100

    Added parameter mapping to controller methods.

--- mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.net.URI;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class AreaController {
     private MessageQueue messageQueue;
 
     @PostMapping(value="/{uuid}", produces=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ObjectNode> createUploadArea(String submissionUuid) {
+    public ResponseEntity<ObjectNode> createUploadArea(@PathParam("uuid") String submissionUuid) {
         LOGGER.info(format("Upload Area creation requested for [%s]...", submissionUuid));
         ObjectNode response = objectMapper.createObjectNode();
         String uploadAreaUuid = UUID.randomUUID().toString();
@@ -39,8 +40,9 @@ public class AreaController {
         return ResponseEntity.created(URI.create(uploadAreaUri)).body(response);
     }
 
-    @PutMapping("/{uploadAreaUuid}/{fileName}/validate")
-    public ResponseEntity validateFile(String areaUuid, String fileName) {
+    @PutMapping("/{areaUuid}/{fileName}/validate")
+    public ResponseEntity validateFile(@PathParam("areaUuid") String areaUuid,
+            @PathParam("fileName") String fileName) {
         LOGGER.info(format("File validation requested for [%s in %s]...", fileName, areaUuid));
         String validationId = UUID.randomUUID().toString();
         ObjectNode response = objectMapper.createObjectNode();
