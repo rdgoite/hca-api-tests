2f788fcb9a8a989e09e8c04efe7a4514b6e0e2b6
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-15 12:00:25 +0100

    Updated test script to mock uploading of files.

--- locust/secondary_analysis.py
+++ locust/secondary_analysis.py
@@ -13,6 +13,8 @@ AUTH_BROKER_URL = 'https://danielvaughan.eu.auth0.com/oauth/token'
 BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 FILE_DIRECTORY = f'{BASE_DIRECTORY}/files/secondary_analysis'
 
+DEFAULT_FILE_UPLOAD_URL = 'http://localhost:8888/v1'
+
 _config = ConfigParser()
 _secret_file_path = os.path.join(BASE_DIRECTORY, 'secrets.ini')
 _config.read(_secret_file_path)
@@ -75,6 +77,11 @@ for name in ['ERR1630013.fastq.gz', 'ERR1630014.fastq.gz']:
     _dummy_analysis_files.append(test_file)
 
 
+_file_upload_base_url = os.environ.get('FILE_UPLOAD_URL')
+if not _file_upload_base_url:
+    _file_upload_base_url = DEFAULT_FILE_UPLOAD_URL
+
+
 class SecondarySubmission(TaskSet):
 
     _access_token = None
@@ -86,8 +93,7 @@ class SecondarySubmission(TaskSet):
     def setup_analysis(self):
         submission = self._create_submission()
         if submission:
-            processes_link = submission.get_link('processes')
-            self._add_analysis_to_submission(processes_link)
+            self._add_analysis_to_submission(submission)
 
     def _create_submission(self) -> Resource:
         headers = {'Authorization': f'Bearer {self._access_token}'}
@@ -101,7 +107,8 @@ class SecondarySubmission(TaskSet):
             _submission_queue.queue(submission)
         return submission
 
-    def _add_analysis_to_submission(self, processes_link):
+    def _add_analysis_to_submission(self, submission: Resource):
+        processes_link = submission.get_link('processes')
         response = self.client.post(processes_link, json=_dummy_analysis,
                                     name='add analysis to submission')
         analysis_json = response.json()
@@ -110,12 +117,33 @@ class SecondarySubmission(TaskSet):
             analysis = Resource(links)
             _analysis_queue.queue(analysis)
             self._add_file_reference(analysis)
+            self._upload_files(submission)
 
     def _add_file_reference(self, analysis: Resource):
         file_reference_link = analysis.get_link('add-file-reference')
         for dummy_analysis_file in _dummy_analysis_files:
             self.client.put(file_reference_link, json=dummy_analysis_file,
                             name="add file reference")
+
+    def _upload_files(self, submission: Resource):
+        upload_area_uuid = None
+        submission_link = submission.get_link('self')
+        while not upload_area_uuid:
+            upload_area_uuid = self._get_upload_area_uuid(submission_link)
+        for _dummy_analysis_file in _dummy_analysis_files:
+            upload_url = f'{_file_upload_base_url}/area/{upload_area_uuid}/files'
+            file_json = {'fileName': _dummy_analysis_file['fileName'], 'contentType': 'fastq'}
+            requests.put(upload_url, json=file_json)
+
+    def _get_upload_area_uuid(self, submission_link):
+        upload_area_uuid = None
+        submission_data = self.client.get(submission_link, name='get submission data').json()
+        staging_details = submission_data.get('stagingDetails')
+        if staging_details:
+            staging_area_uuid = staging_details.get('stagingAreaUuid')
+            if staging_area_uuid:
+                upload_area_uuid = staging_area_uuid.get('uuid')
+        return upload_area_uuid
 
 
 class GreenBox(HttpLocust):
