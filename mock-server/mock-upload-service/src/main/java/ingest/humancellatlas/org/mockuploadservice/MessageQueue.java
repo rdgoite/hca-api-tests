a6134513b8e4de933649d160a7019d25987eaaa2
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-30 16:01:57 +0100

    Updated the mock validation message.

--- mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/MessageQueue.java
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/MessageQueue.java
@@ -1,5 +1,6 @@
 package ingest.humancellatlas.org.mockuploadservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,6 +19,15 @@ public class MessageQueue {
     public static final String EXCHANGE_FILE_STAGED = "ingest.file.staged.exchange";
     public static final String ROUTING_KEY_FILE_STAGED = "ingest.file.create.staged";
 
+
+    private static final String DEFAULT_VALIDATION_OUTPUT;
+    static {
+        StringBuilder outputBuilder = new StringBuilder("{");
+        outputBuilder.append("\"validation_errors\": [], \"validation_state\": \"\"");
+        outputBuilder.append("}");
+        DEFAULT_VALIDATION_OUTPUT = outputBuilder.toString();
+    }
+
     @Autowired
     private ConnectionFactory connectionFactory;
 
@@ -30,7 +40,7 @@ public class MessageQueue {
     }
 
     public void sendValidationStatus(ObjectNode validationResult) {
-        validationResult.putObject("stdout").putArray("validationErrors");
+        validationResult.put("stdout", DEFAULT_VALIDATION_OUTPUT);
         rabbitTemplate.convertAndSend(EXCHANGE_FILE_VALIDATION, ROUTING_KEY_FILE_VALIDATION,
                 validationResult);
     }
